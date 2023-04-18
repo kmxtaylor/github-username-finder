b1f5d6fab52e2f2cc946fd5a13c1e8384e8c75fb
Katie Taylor <katiemxtaylor@gmail.com>
2023-04-18 10:26:54 -0700

    load fonts globally
    currently not using splashscreen

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
+import { useFonts, useCallBack } from 'expo-font';
+
+
 import GitHubProfile from './screens/GitHubProfile';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    'SpaceMono-Regular': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    'SpaceMono-Bold': require('./assets/fonts/SpaceMono-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+
   return <GitHubProfile />;
 };
 
