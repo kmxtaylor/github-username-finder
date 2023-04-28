7af09330fb2c047b06462430eeb89e4f41375f72
Katie Taylor <katiemxtaylor@gmail.com>
2023-04-27 17:57:46 -0700

    remove commented out functionality

--- screens/GitHubProfile/index.js
+++ screens/GitHubProfile/index.js
@@ -36,35 +36,6 @@ const GitHubProfile = () => {
       <Card user={user} error={error} loading={loading} />
     </View>
   );
-
-  // return (
-  //   <View style={{ paddingHorizontal: 20 }}>
-  //     <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>GitHub Profile Search</Text>
-  //     <TextInput
-  //       style={{ borderWidth: 1, borderColor: '#ccc', padding: 5, marginBottom: 10 }}
-  //       value={username}
-  //       onChangeText={setUsername}
-  //       placeholder="Enter GitHub username"
-  //       autoCapitalize="none"
-  //       autoCorrect={false}
-  //     />
-  //     <Button title="Search" onPress={searchUser} disabled={!username} />
-
-  //     {loading ? (
-  //       <ActivityIndicator size="large" style={{ marginTop: 20 }} />
-  //     ) : user ? (
-  //       <Card containerStyle={{ marginTop: 20 }}>
-  //         <Image source={{ uri: user.avatar_url }} style={{ width: 200, height: 200, alignSelf: 'center' }} />
-  //         <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 20, textAlign: 'center' }}>{user.login}</Text>
-  //         <Text style={{ marginTop: 10, textAlign: 'center' }}>{user.name || '-'}</Text>
-  //         <Text style={{ marginTop: 10, textAlign: 'center' }}>{user.bio || '-'}</Text>
-  //         <Text style={{ marginTop: 10, textAlign: 'center' }}>{user.location || '-'}</Text>
-  //       </Card>
-  //     ) : error ? (
-  //       <Text style={{ marginTop: 20, color: 'red' }}>{error}</Text>
-  //     ) : null}
-  //   </View>
-  // );
 };
 
 export default GitHubProfile;
