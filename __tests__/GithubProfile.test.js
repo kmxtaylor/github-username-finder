c8a3ebd6bd9887fdd5c7ca6a39f1896ea59dbd00
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-20 21:06:15 -0700

    add test for preloading profile
    also add jest set-up file to fix jest compatibility issues w/ npm packages

---
+++ __tests__/GithubProfile.test.js
@@ -0,0 +1,22 @@
+import { render, waitFor } from '@testing-library/react-native';
+import App from '../App';
+import GitHubProfile from '../src/screens/GitHubProfile';
+
+const TIMEOUT = { timeout: 10000 };
+
+// test if the app renders correctly without crashing
+test('renders correctly', () => {
+  render(<App />);
+});
+
+// test if Github Profile can preload a profile of @octocat
+test('preloads octocat profile', async () => {
+  const { getByText, getByTestId } = render(<GitHubProfile />);
+
+  await waitFor(() => {
+    // getByText('@octocat')
+    expect(getByText('San Francisco')).toBeTruthy();
+  }, TIMEOUT);
+
+
+});
