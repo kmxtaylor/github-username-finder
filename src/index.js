e4c3b9f5184a272cd105e965bcd4a797d810c3ab
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-20 22:37:50 -0700

    reorganize outer wrappers + fix main background color switching
    not all components are color-adjusted for theme yet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'contexts/Theme';
 import RootNavigation from './navigation/RootNavigation';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <RootNavigation />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <RootNavigation />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 };
 
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,20 +1,26 @@
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { StatusBar } from 'components/themed';
 
+import { useThemeColors } from 'hooks/useThemeColors';
+
 const Main = ({ children }) => {
+  const { colors } = useThemeColors();
+
   return (
     <>
       <StatusBar />
-      <SafeAreaProvider style={styles.container}>
-        <ScrollView
-          style={styles.subContainer}
-          keyboardShouldPersistTaps='handled'
-        >
-          {children}
-        </ScrollView>
-      </SafeAreaProvider>
+      {/* <SafeAreaProvider
+        style={[styles.container, {backgroundColor: colors.background}]}
+      > */}
+      {/* may need to adjust element layering */}
+      <SafeAreaView
+        style={[styles.container, {backgroundColor: colors.background}]}
+      >
+        {children}
+      </SafeAreaView>
+      {/* </SafeAreaProvider> */}
     </>
   );
 };
@@ -22,7 +28,7 @@ const Main = ({ children }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#141d2f',
+    // backgroundColor: '#141d2f',
   },
   subContainer: {
     padding: 20,
